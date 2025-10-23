78399d97eeecc4f958b18216ac6f70d1747ccced
Qianhui-Lin <qianhuilin33@gmail.com>
2025-10-23 09:43:22 +0100

    included database initialisation for monolith

--- database/database_initialize.py
+++ database/database_initialize.py
@@ -3,11 +3,13 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 soil_db = client['soil_database']
 plotting_db = client['plotting_database']
+monolith_db = client['monolith_database']
 
 
 # Clear existing collections
 
 soil_db['soil_profiles'].delete_many({})
 plotting_db['plotting'].delete_many({})
+monolith_db['plotting_monolith'].delete_many({})
 
 print("Database has been initialized and collections cleared.")
